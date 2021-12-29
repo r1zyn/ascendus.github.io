ed8d07c530724c5a7fefc3b786e818990e98e3ec
Ascendus <ascendus.dev@gmail.com>
2021-12-29 14:17:48 +1300

    feat: Updated configuration and added nojekyll file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 /** @type {import("next").NextConfig} */
 module.exports = {
+    assetPrefix: isProduction ? "/ascendus.github.io" : "",
     reactStrictMode: true
 }
