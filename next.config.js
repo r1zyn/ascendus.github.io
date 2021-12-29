ddc6c3eeda0ae17f036e7cc28c5b295e723bb1ea
Ascendus <ascendus.dev@gmail.com>
2021-12-29 16:14:50 +1300

    chore: Set custom loader option in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const isProduction = process.env.NODE_ENV === "production";
 /** @type {import("next").NextConfig} */
 module.exports = {
     assetPrefix: isProduction ? "/ascendus.github.io/" : "",
+    images: {
+        loader: "custom"
+    },
     reactStrictMode: true
 }
