fd6c8184eaee8216952e1b5d135c621103bff4d1
Ascendus <ascendus.dev@gmail.com>
2022-01-27 19:40:42 +1300

    fix: updated next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ module.exports = (phase, { defaultConfig }) => {
         /** @type {import("next").NextConfig} */
         const nextDevConfig = {
             assetPrefix: "http://localhost:3000/",
-            basePath: "./",
             pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
             reactStrictMode: true
         }
@@ -15,7 +14,6 @@ module.exports = (phase, { defaultConfig }) => {
         /** @type {import("next").NextConfig} */
         const nextProdConfig = {
             assetPrefix: "https://cdn.statically.io/gh/Ascendus/ascendus.github.io/main/",
-            basePath: "./",
             pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
             reactStrictMode: true
         }
